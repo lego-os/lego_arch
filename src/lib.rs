e656a8a732ad9bedb1a7ba68365bc5bd0e070dfe
QIUZHILEI <zhilei.oerv@isrc.iscas.ac.cn>
2025-02-05 18:46:59 +0800

    修改use作用域
    Signed-off-by: QIUZHILEI <zhilei.oerv@isrc.iscas.ac.cn>

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,7 @@
 #![no_std]
 mod arm;
 #[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
-pub mod riscv;
+mod riscv;
+
+#[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
+pub use riscv::*;
